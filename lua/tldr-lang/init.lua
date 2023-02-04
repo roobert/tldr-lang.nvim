ac6132dc4769690f93eccad091a33b8895dcc756
Rob Wilson <roobert@gmail.com>
2023-02-04 23:25:06 +0000

    Add basic ability to open file in split

--- lua/tldr-lang/init.lua
+++ lua/tldr-lang/init.lua
@@ -32,22 +32,48 @@ M.tldr = function()
 		-- * render buffer as markdown
 		-- * add some function or command to automatically add new tldr docs..
 
+		if not type then
+			print("[" .. filetype .. "] failed opening tldr-lang for unknown: " .. first_line)
+			return
+		end
+
 		if type == "list" or type == "array" then
 			print("[" .. filetype .. "] opening array tldr..")
+			file = "array"
 		elseif type == "dict" then
 			print("[" .. filetype .. "] opening dict tldr..")
+			file = "dict"
 		elseif type == "string" or type == "Literal" or type == "str" then
 			print("[" .. filetype .. "] opening string tldr..")
+			file = "string"
 		else
 			P(type)
 			print("[" .. filetype .. "] failed opening tldr-lang for unknown: " .. first_line)
 		end
 
-		-- local cmd = string.format("tldr %s", typ)
-		-- vim.cmd(cmd)
+		local info = debug.getinfo(1, "S")
+		local script_path = info.source:sub(2)
+		local plugin_dir = script_path:match("(.*/)")
 
-		-- use this or split..
-		--vim.cmd("ped")
+		--print("split " .. plugin_dir .. "../../doc/" .. filetype .. "/" .. file .. ".md")
+		vim.api.nvim_command("split " .. plugin_dir .. "../../doc/" .. filetype .. "/" .. file .. ".md")
+
+		-- split +/Users/rw/.config/nvim/lua/tldr-lang/doc/python/.md
+
+		-- local win_id = vim.api.nvim_get_current_win()
+		-- local buf_id = vim.api.nvim_get_current_buf()
+
+		-- vim.api.nvim_buf_set_name(buf_id, "TLDR-Lang")
+		-- vim.api.nvim_set_option_value("wrap", false, { win = win_id })
+		-- vim.api.nvim_set_option_value("cursorline", false, { win = win_id })
+		-- vim.api.nvim_set_option_value("number", false, { win = win_id })
+		-- vim.api.nvim_set_option_value("relativenumber", false, { win = win_id })
+
+		-- vim.api.nvim_set_option_value("buftype", "nofile", { buf = buf_id })
+		-- vim.api.nvim_set_option_value("swapfile", false, { buf = buf_id })
+		-- vim.api.nvim_set_option_value("bufhidden", "wipe", { buf = buf_id })
+		-- vim.api.nvim_set_option_value("filetype", "nvim-search-replace", { buf = buf_id })
+		-- vim.api.nvim_set_option_value("modifiable", false, { buf = buf_id })
 	end)
 end
 
